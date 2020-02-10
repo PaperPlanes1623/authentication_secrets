de7a334068f9083be5c25d084dc08a02167522bf
Dylan Taft <dylbronjames@gmail.com>
2020-02-10 15:21:12 -0700

    added user register and login

--- app.js
+++ app.js
@@ -2,8 +2,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
+//connect to mongodb
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
+//create user schema with two fields
+const userSchema = {
+  email: String,
+  password: String
+};
+
+const User = new mongoose.model("User", userSchema);
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -19,6 +29,38 @@ app.get("/login", function (req, res) {
 
 app.get("/register", function (req, res) {
   res.render("register");
+});
+
+app.post("/register", function (req, res) {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+
+  newUser.save(function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("secrets");
+    }
+  });
+});
+
+app.post("/login", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({ email: username }, function (err, foundUser) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        if (foundUser.password === password) {
+          res.render("secrets");
+        }
+      }
+    }
+  })
 });
 
 app.listen(3000, function () {
