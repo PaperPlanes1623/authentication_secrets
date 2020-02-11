98a57e0c9b8ceaab2ceb662cece215ae1c17b6ff
Dylan Taft <dylbronjames@gmail.com>
2020-02-11 13:07:13 -0700

    add environment vars

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -6,6 +7,9 @@ const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
 
 const app = express();
+
+console.log(process.env.API_KEY);
+
 //connect to mongodb
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 //create user schema with two fields
@@ -14,8 +18,7 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-const secret = "Thisisourlittlesecret.";
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
